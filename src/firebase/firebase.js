fe5896744eacd75cefef7363ef6eea0ba93e7c37
Thomas DoyleEngler <tpdoyle87@gmail.com>
2018-12-31 19:10:37 -0500

    add firebase

---
+++ src/firebase/firebase.js
@@ -0,0 +1,15 @@
+import * as firebase from 'firebase'
+
+const config = {
+  apiKey: "AIzaSyCEHg-TPsmFMCYS9Acfgpd8IGRxCR_TjjY",
+  authDomain: "expensify-c68da.firebaseapp.com",
+  databaseURL: "https://expensify-c68da.firebaseio.com",
+  projectId: "expensify-c68da",
+  storageBucket: "expensify-c68da.appspot.com",
+  messagingSenderId: "487281435737"
+};
+firebase.initializeApp(config);
+
+firebase.database().ref().set({
+  name: "Thomas Doyle-Engler"
+})
--- src/index.js
+++ src/index.js
@@ -14,6 +14,8 @@ import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/initialize';
 import 'react-dates/lib/css/_datepicker.css';
+// database
+import './firebase/firebase'
 
 
 
